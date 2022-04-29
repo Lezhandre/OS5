e7ca9a74e7e280d8796e1d8e627c931d9d3f1056
Lezhandre <duzhenko.misha@mail.ru>
2022-04-29 14:08:40 +0300

    I've forgotten new line

--- main.c
+++ main.c
@@ -58,7 +58,7 @@ void *bump(void* arg) {
 
 int main(int argc, char* argv[]) {
     if (argc == 2 && strcmp(argv[1], "--help") == 0) {
-        printf("Right input: ./main \"picture.bmp\" number_of_threads");
+        printf("Right input: ./main \"picture.bmp\" number_of_threads\n");
         exit(EXIT_SUCCESS);
     }
     if (argc != 3) {
